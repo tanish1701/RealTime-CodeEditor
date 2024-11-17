aadb798b088ccbe7577c077cbc052c61de8810cc
Tanish Mehta <tanish@Tanishs-MacBook-Air.local>
2024-11-17 16:36:52 +0530

    Fix react-scripts issue and specify Node.js version

--- server.js
+++ server.js
@@ -93,5 +93,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
